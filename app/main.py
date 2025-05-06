d056850f9f7586ed209a284778fc3c8f71bcd621
lhy <myweethon@outlook.com>
2025-05-06 10:07:17 +0800

    feat: user register login info

--- app/main.py
+++ app/main.py
@@ -1,37 +1,39 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import migrate
+from flask_migrate import Migrate
 
-from app.api.endpoints import chat, search, document
+from app.api.endpoints import chat, search, document, auth
 from app.config import Settings
 from app.db.milvus_client import milvus_client
 from app.db.neo4j_client import neo4j_client
+from app.extensions import db
 
 app = Flask(__name__)
-db = SQLAlchemy()
-
 
 @app.route('/')
 def home():
-    #using as a health check
     return 'Hello, World!'
 
 
 def create_app():
+    set_env()
+    app.config.from_object(Settings)
+
     db.init_app(app)
-    migrate.init_app(app, db)
+    Migrate(app, db)
+
     app.register_blueprint(chat.bp)
     app.register_blueprint(search.bp)
     app.register_blueprint(document.bp)
+    app.register_blueprint(auth.bp)
     milvus_client.connect(app)
     neo4j_client.connect(app)
-    set_env()
     CORS(app, supports_credentials=True)
     return app
 
 def set_env():
-    import os
     os.environ['LANGSMITH_ENDPOINT'] = Settings.LANGSMITH_ENDPOINT
     os.environ['LANGSMITH_API_KEY'] = Settings.LANGSMITH_API_KEY
     os.environ['LANGSMITH_TRACING'] = Settings.LANGSMITH_TRACING
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,14 +1,57 @@
 import uuid
-
-from flask import Blueprint, request, jsonify
-
+import datetime
+import jwt
+from zoneinfo import ZoneInfo
+from app.extensions import db
 from app.models.user import User
-
+from flask import jsonify
+from app.config import Settings
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def register_user(data):
-    username = data['username']
-    password = data['password']
-    email = data['email']
-    user_type = data['user_type']
+    username = data.get('username')
+    password = data.get('password')
+    email = data.get('email')
+    user_type = data.get('user_type', 'normal')
 
-    new_user = User()
+    if User.query.filter((User.username == username) | (User.email == email)).first():
+        return jsonify({'error': 'Username already exists'}), 400
+
+    hashed_password = generate_password_hash(password)
+
+    new_user = User(username=username, password=hashed_password, email=email, user_type=user_type)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'Registration successful! id': new_user.id}), 201
+
+
+def login_user(data):
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if user and check_password_hash(user.password, password):
+        payload = {
+            'user_id': user.id,
+            'user_type': user.user_type,
+            'exp': datetime.datetime.now(ZoneInfo('Asia/Shanghai')) + datetime.timedelta(minutes=30)
+        }
+        token = jwt.encode(payload, Settings.SECRET_KEY, algorithm='HS256')
+        return jsonify({'token': token}), 200
+
+    return jsonify({'error': 'Invalid credentials'}), 401
+
+def get_user_info(jwt_token):
+    try:
+        payload = jwt.decode(jwt_token, Settings.SECRET_KEY, algorithms='HS256')
+        user_id = payload.get('user_id')
+        user_type = payload.get('user_type')
+        user = User.query.get(user_id)
+        if user:
+            return jsonify({'user_id': user.id, 'user_type': user_type, 'email': user.email,'user_name': user.username, 'create_time': user.created_at}), 200
+    except jwt.ExpiredSignatureError:
+        return jsonify({'error': 'Expired token'}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({'error': 'Invalid token'}), 401
+
+    return jsonify({'error': 'User not found'}), 404
